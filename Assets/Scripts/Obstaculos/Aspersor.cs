07ddc298ee8b740fb67db4a6568bdcabdaea14a3
phadzich <phadzich@gmail.com>
2024-12-05 14:30:23 -0500

    Aspersor
    Splash splash

---
+++ Assets/Scripts/Obstaculos/Aspersor.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Aspersor : MonoBehaviour
+{
+    [SerializeField]
+    float intervaloDisparos;
+
+    [SerializeField]
+    float velocidadAgua;
+    [SerializeField]
+    float fuerzaY;
+
+    public bool estaPrendido;
+
+    [SerializeField]
+    GameObject chorroAguaPrefab;
+
+    [SerializeField]
+    Transform[] waterSources;
+
+    void Start()
+    {
+        Prender();
+        intervaloDisparos = 1f;
+    }
+
+
+
+
+    IEnumerator ActivarDisparos()
+    {
+        yield return new WaitForSeconds(intervaloDisparos);
+        if (estaPrendido)
+        {
+
+            DispararAgua();
+        }
+        else
+        {
+            StopCoroutine(ActivarDisparos());
+        }
+    }
+
+        private void Prender()
+    {
+        //Debug.Log("PRENDIDO");
+        estaPrendido = true;
+        StartCoroutine(ActivarDisparos());
+    }
+
+    private void Apagar()
+    {
+        estaPrendido = false;
+        //Debug.Log("APAGADO");
+
+    }
+
+    private void DispararAgua()
+    {
+        //Debug.Log("SPLASH");
+        StartCoroutine(ActivarDisparos());
+
+        
+        var newAgua1 = Instantiate(chorroAguaPrefab, waterSources[2]);
+        Vector3 direccion = this.transform.forward*velocidadAgua;
+        direccion.y = fuerzaY;
+        newAgua1.GetComponent<Rigidbody>().AddForce(direccion, ForceMode.Impulse);
+        
+        direccion = this.transform.forward * -velocidadAgua;
+        direccion.y = fuerzaY;
+        var newAgua3 = Instantiate(chorroAguaPrefab, waterSources[3]);
+        newAgua3.GetComponent<Rigidbody>().AddForce(direccion, ForceMode.Impulse);
+
+        direccion = this.transform.right * -velocidadAgua;
+        direccion.y = fuerzaY;
+        var newAgua4 = Instantiate(chorroAguaPrefab, waterSources[1]);
+      newAgua4.GetComponent<Rigidbody>().AddForce(direccion, ForceMode.Impulse);
+
+        direccion = this.transform.right * velocidadAgua;
+        direccion.y = fuerzaY;
+
+        var newAgua2 = Instantiate(chorroAguaPrefab, waterSources[0]);
+      newAgua2.GetComponent<Rigidbody>().AddForce(direccion, ForceMode.Impulse);
+
+    }
+}
