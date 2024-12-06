cb7e6b214c379d75eb16ce2b93dbd8b271791eb7
Juani0814 <juanitamarulandarguello@gmail.com>
2024-12-05 19:53:53 -0500

    Movimiento de cámara
    Se mueve, salta y la cámara lo sigue de manera horizontal

--- Assets/WIP/JUANI/PET_MOVEMENT/PetController.cs
+++ Assets/WIP/JUANI/PET_MOVEMENT/PetController.cs
@@ -5,54 +5,52 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PetController : MonoBehaviour
 {
-    CharacterController Controller;
-    //Animator animator;
-    [SerializeField] float speed = 6f;
-    //[SerializeField] float rotspeed = 3f
+    public CharacterController Controller;
+    public Transform cam;
+
+    public float speed = 0.5f;
     Vector2 moveInput;
     Vector3 movement;
-
-    [SerializeField] float jumpspeed = 0.2f;
-    [SerializeField] float jumpgravity = 0.75f;
-    bool jumping = false;
+    public float smoothingTime = 0.1f;
+    float smoothingVelocity;
     
+    [SerializeField] float jumpspeed = 16f;
+    [SerializeField] float gravity = -9.81f;
+    private Vector3 velocity; 
+    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Controller = GetComponent<CharacterController>();
-        //animator = GetComponent<Animator>();
     }
 
-    void OnMove(InputValue value) { 
-        moveInput = value.Get<Vector2>();
-    }
-    void OnJump() {
-        if (Controller.isGrounded && !jumping) {
-            jumping = true;
+    void Update() {
+
+        isGrounded = Controller.isGrounded; 
+        if (isGrounded && velocity.y < 0) { 
+            velocity.y = -2f; 
         }
-    }
 
-    private void FixedUpdate()
-    {
-        movement.x = moveInput.x * speed;
-        movement.z = moveInput.y * speed;
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        if (Controller.isGrounded) {
-            movement.y = -jumpgravity * 0.1f;
-            if (jumping) {
-                movement.y = jumpspeed;
-                jumping = false;
-            }
-        } else { 
-            movement.y -= jumpgravity * (movement.y > 0  ? 2.25f : 2.75f);
+        if (direction.magnitude > 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothingVelocity, smoothingTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Controller.Move(moveDir.normalized * speed * Time.fixedDeltaTime);
         }
-        //if (direction.magnitude > 0.1f) {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-            Controller.Move(movement * Time.fixedDeltaTime);
-        //}
+        velocity.y += gravity * Time.deltaTime * 2;
+        Controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnJump() { 
+        if (isGrounded) { 
+            velocity.y = Mathf.Sqrt(jumpspeed * -0.2f * gravity); 
+        } 
     }
 }
