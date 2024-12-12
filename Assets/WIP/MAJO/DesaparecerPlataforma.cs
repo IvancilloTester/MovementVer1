083854bf1851961eda359dfa5b51de660b96049e
María José Burguete Euán <marijo.mjbe@gmail.com>
2024-12-12 01:44:20 -0600

    2do Avance en Blocking V2
    * Se agregaron las plataformas que desaparecen
    * Se arreglo el script para abrir las puertas.
    * Se eliminaron algunos componentes como el animator de la puerta.

---
+++ Assets/WIP/MAJO/DesaparecerPlataforma.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class DesaparecerPlataforma : MonoBehaviour
+{
+
+    public float timeToDestroy = 3f;// Tiempo que tiene el jugador antes de que la plataforma desaparezca, debe ser mayor al tiempo del parpadeo
+    public float timeToReappear = 4f;// Tiempo que tarda la plataforma en reaparecer
+    public float blinkingInterval = 0.5f; // Intervalo de tiempo en el que se repite el parpadeo
+    public float blinkingTime = 2.5f; // Tiempo del parpadeo
+
+    private MeshRenderer meshRenderer;
+    private Collider platformCollider;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        platformCollider = GetComponent<Collider>();
+    }
+
+    // Hace la plataforma visible e invisible para simular un parpadeo
+    void Blinking()
+    {
+        meshRenderer.enabled = !meshRenderer.enabled; // Alterna entre visible e invisible
+    }
+
+    // Se llama cuando se requiere detener el parpadeo
+    void StopBlinking()
+    {
+        CancelInvoke(nameof(Blinking)); // Detiene el parpadeo
+        meshRenderer.enabled = true; // Nos aseguramos de que sea visible antes de desaparecer
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            InvokeRepeating(nameof(Blinking), 0f, blinkingInterval); // Se empieza el parpadeo llamando repetidamente la funcion Blinking desde el inicio en intervalors de blinkingInterval segundos.
+            Invoke(nameof(StopBlinking), blinkingTime); // Se detiene el parpadeo despues de cierto tiempo (blinkingTime).
+            Invoke(nameof(PlatformDisappears), timeToDestroy); // La plataforma desaparece despues de cierto tiempo (timeToDestroy), ocurre al mismo tiempo que el parpadeo
+        }
+    }
+
+    // Desactiva el mesh renderer y el collider de la plataforma
+    void PlatformDisappears()
+    {
+        meshRenderer.enabled = false;
+        platformCollider.enabled = false;
+
+        Invoke(nameof(PlatformAppears), timeToReappear); // La plataforma regresa a su estado normal despues de timeToReappear segundos.
+    }
+
+
+    // Reactiva el mesh renderer y el collider de la plataforma.
+    void PlatformAppears()
+    {
+        meshRenderer.enabled = true;
+        platformCollider.enabled = true;
+    }
+}
