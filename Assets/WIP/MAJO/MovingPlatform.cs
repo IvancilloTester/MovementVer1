668831bb91dda218e14dbc0f6da460e1d38ea67f
María José Burguete Euán <marijo.mjbe@gmail.com>
2024-12-13 17:54:43 -0600

    Comentarios en Scripts
    Se agregaron comentarios en los scripts relacionados a las plataformas que se mueven.

--- Assets/WIP/MAJO/MovingPlatform.cs
+++ Assets/WIP/MAJO/MovingPlatform.cs
@@ -20,35 +20,38 @@ public class MovingPlatform : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        TargetNextWaypoint();
+        TargetNextWaypoint(); // Se llama para iniciar el movimiento
     }
 
     // Update is called once per frame
     void Update()
     {
-        _elapsedTime += Time.deltaTime;
+        _elapsedTime += Time.deltaTime; // Va actualizando el tiempo recorrido
 
-        float elapsedPercentage = _elapsedTime / _timeToWaypoint;
-        elapsedPercentage = Mathf.SmoothStep(0, 1, elapsedPercentage);
-        transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
+        float elapsedPercentage = _elapsedTime / _timeToWaypoint; // Obtenemos el porcentaje del camino recorrido, diviendo el tiempo recorrido por el tiempo total calculado anteriormente
+        elapsedPercentage = Mathf.SmoothStep(0, 1, elapsedPercentage); // Modifica el progreso para que este no se mueva con velocidad constante si no que suaviza el moviemiento al inicio y al final
+        transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage); // Calcula la posicion entre dos puntos basada en un porcentaje de "progreso" (elapsedPercentage)
 
+        // Si el porcentaje ha llegado a 1 o es mayor a 1, significa que ya ha llegado a su punto destino por lo cual
+        // llamamos a TargetNextWaypoint para seguir la ruta.
         if (elapsedPercentage >= 1)
         {
             TargetNextWaypoint();
         }
     }
 
+    // Obtiene el siguiente punto al cual debe de avanzar la plataforma
     private void TargetNextWaypoint()
     {
-        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
-        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
-        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex); // Se actualiza para que el punto actual, ahora sea guardado como el punto anterior
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex); // Se actualiza el punto actual para que tenga el indice del siguiente punto
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex); // Se actualiza el game object con el punto al que ahora se dirije
 
 
-        _elapsedTime = 0;
+        _elapsedTime = 0; // Se reinicia el tiempo recorrido
 
-        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
-        _timeToWaypoint = distanceToWaypoint / _speed;
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position); // Se calcula la distancia entre el punto actual y el siguiente
+        _timeToWaypoint = distanceToWaypoint / _speed; // Se calcula el tiempo total que toma ir de previousWaypoint al targetWaypoint | tiempo = distancia / velocidad
 
 
 
--- Assets/WIP/MAJO/WaypointPath.cs
+++ Assets/WIP/MAJO/WaypointPath.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class WaypointPath : MonoBehaviour
 {
 
+    // Regresa un Waypoint a partir de su indice, donde los Waypoints son los hijos del objeto que tiene este script
     public Transform GetWaypoint(int waypointIndex)
     {
         return transform.GetChild(waypointIndex);
     }
 
-
+    // Regresa el indice del siguiente Waypoint en la ruta y se asegura de reiniciar la ruta una vez que se haya recorrido por completo.
     public int GetNextWaypointIndex(int currentWaypointIndex)
     {
         int nextWaypointIndex = currentWaypointIndex + 1;
