415d9478ffbbfcc83db6f35f1c488b51fac8133a
Juani0814 <juanitamarulandarguello@gmail.com>
2024-12-14 23:11:30 -0500

    Enemigo
    El personaje regresa al checkpoint y pierde una vida cuando entra al rango del enemigo

--- Assets/WIP/JUANI/PET_MOVEMENT/Enemy.cs
+++ Assets/WIP/JUANI/PET_MOVEMENT/Enemy.cs
@@ -9,27 +9,21 @@ public class Enemy : MonoBehaviour
 {
     public Animator animator;
     public Estados estado;
-    public float distanciaVer = 3;
+    public float distanciaVer = 3.0f;
     public bool AutoSelectTarget = true;
 
     public Transform Target;
     public float distancia;
     public float waitTime = 2.0f;
 
-    //[SerializeField]
-    //private int damageValue;
-
+    /* Hace un target en el jugador y empieza la corutina de calcular la distancia*/
     private void Awake() {
-        if (AutoSelectTarget)
-        {
+        if (AutoSelectTarget) {
             Target = GameObject.FindGameObjectWithTag("Player").transform;
         }
         StartCoroutine(CalcularDistancia());
     }
 
-    //void Update() {
-    //    Animate();
-    //}
     private void LateUpdate() {
         CheckEstado();
     }
@@ -68,10 +62,15 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    /* Hace la animación del estado idle del enemigo, además si el
+       personaje entra en el área del enemigo, este hace una animación.
+       Después de un waitTime, te quita una vida y regresas al checkpoint */
     public virtual void IdleEstado() {
         animator.SetInteger("Cambios", (int)estado);
-        if (distancia < distanciaVer) {
+        if (distancia <= distanciaVer) {
             CambioEstado(Estados.muerto);
+            animator.SetInteger("Cambios", (int)estado);
+            Invoke("AccionEnemigo", waitTime);
         }
     }
 
@@ -80,23 +79,24 @@ public class Enemy : MonoBehaviour
 
     }
 
+    /* Si el personaje sale del rango del enemigo, este vuelve al estado Idle */
     public virtual void MuertoEstado() {
-        animator.SetInteger("Cambios", (int)estado);
-        StartCoroutine(MuertoEstado2());
+        if (distancia > distanciaVer)
+        {
+            CambioEstado(Estados.idle);
+        }
+        
     }
 
-    private IEnumerator MuertoEstado2() {
-        yield return StartCoroutine(Esperar());
+    /* Remueve una vida del personaje y lo devuelve a un checkpoint 
+       la puse así porque usé invoke para esperar un waitTime antes 
+       de realizar esta acción */
+    private void AccionEnemigo() {
         GameManager.instance.playerStats.RemoveVidas(1);
-        CambioEstado(Estados.idle);
+        Debug.Log("Enemigo");
     }
 
-
-
-    IEnumerator Esperar() {
-        yield return new WaitForSeconds(waitTime);
-    }
-
+    /* Calcula la distancia a la que el personaje se encuentra del enemigo */
     IEnumerator CalcularDistancia() {
         while (true) {
             if (Target != null) { 
@@ -106,6 +106,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
+/* Solo se ve en el editor, y es el área en que el enemigo te descubre */
 #if UNITY_EDITOR
     private void OnDrawGizmosSelected() {
         Handles.color = Color.blue;
@@ -113,11 +114,9 @@ public class Enemy : MonoBehaviour
     }
 #endif
 
-    //private void Animate() { 
-    //    animator.SetInteger("Cambios", (int)estado);
-    //}
 }
 
+/* Se inicializan los estados en los que puede estar el enemigo */
 public enum Estados { 
     idle        = 0,
     distraido   = 1,
