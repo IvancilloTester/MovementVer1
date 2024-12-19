556e68be5c488d7454d8adeaccbb7a34493dc2c6
Juani0814 <juanitamarulandarguello@gmail.com>
2024-12-19 00:49:53 -0500

    Enemigo
    El enemigo se mueve de manera aleatoria.
    El rota, camina o se queda quieto.
    No se pega a las paredes.
    Realiza perfectamente los cambios de estado en caso de que se encuentre al personaje.

--- Assets/WIP/JUANI/PET_MOVEMENT/Enemy.cs
+++ Assets/WIP/JUANI/PET_MOVEMENT/Enemy.cs
@@ -16,12 +16,75 @@ public class Enemy : MonoBehaviour
     public float distancia;
     public float waitTime = 2.0f;
 
+    public float speedMov = 1.0f;
+    public float speedRot = 50.0f;
+    public int reactTime = 1;
+    private int movement = 0;
+    bool wait, rotate, walk;
+    public float distance = 2.5f;
+
+    private void Start() {
+        action();
+    }
+
     /* Hace un target en el jugador y empieza la corutina de calcular la distancia*/
     private void Awake() {
         if (AutoSelectTarget) {
             Target = GameObject.FindGameObjectWithTag("Player").transform;
         }
         StartCoroutine(CalcularDistancia());
+    }
+
+    private void Update()
+    {
+        if ((int)estado == 0)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, distance)) {
+                if (hit.collider) { 
+                    rotate = true;
+                    StartCoroutine(RotTime());
+                }
+            }
+            if (walk)
+            {
+                animator.SetInteger("Cambios", 1);
+                transform.position += (transform.forward * speedMov * Time.deltaTime);
+            }
+            if (rotate)
+            {
+                //animator.SetInteger("Cambios", 1);
+                transform.Rotate(Vector3.up * speedRot * Time.deltaTime);
+
+            }
+            if (wait)
+            {
+                animator.SetInteger("Cambios", 0);
+            }
+        }
+    }
+
+    void action() {
+        movement = Random.Range(1, 4);
+        if (movement == 1) {
+            wait = false; 
+            walk = true;
+        }
+        if (movement == 2) {
+            wait = true;
+            walk = false;
+        }
+        if (movement == 3) {
+            rotate = true;
+            StartCoroutine(RotTime());
+        }
+        Invoke("action", reactTime);
+    }
+
+    IEnumerator RotTime() {
+        animator.SetInteger("Cambios", 1);
+        yield return new WaitForSeconds(2f);
+        rotate = false;
     }
 
     private void LateUpdate() {
