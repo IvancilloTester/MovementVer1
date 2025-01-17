da84d370c8ef2479f43f090cd056ccd214c5125e
María José Burguete Euán <marijo.mjbe@gmail.com>
2025-01-17 02:53:46 -0600

    Bug Fix: Botón y agua
    Se arreglo la "animación" del botón al ser presionado. El agua ahora quita vidas correctamente.

--- Assets/Scripts/Plataformas/TriggerButton.cs
+++ Assets/Scripts/Plataformas/TriggerButton.cs
@@ -5,10 +5,16 @@ public class TriggerButton : MonoBehaviour
 {
 
     [SerializeField]
-    Material onMaterial;
+    private Material onMaterial;
     [SerializeField]
-    GameObject buttonObject;
+    private GameObject buttonObject;
+    [SerializeField]
+    private float _speed = 2f; // Velocidad del movimiento.
+    [SerializeField]
+    private float _pushDistance = 0.1f; // Distancia que se empuja el boton
 
+    private Vector3 _pushedPos; // Posición del botón presionado
+    private bool _isPushed = false; // Indica si el botón ha sido presionado 
 
     // Interfaz para hacer un objeto "interactuable" (Puertas o puentes)
     public interface IInteractable
@@ -36,14 +42,18 @@ public class TriggerButton : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             interactable?.Interact();
-            PushButton();
+            if (!_isPushed){ // Nos aseguramos de que el boton no haya sido presionado aún.
+                PushButton();
+            }
+            
         }
     }
 
     private void PushButton()
     {
-        Transform _buttonPos = buttonObject.transform;
-        _buttonPos.position = new Vector3(_buttonPos.position.x, 0.1f, _buttonPos.position.z);
-        buttonObject.GetComponent<MeshRenderer>().material = onMaterial;
+        _pushedPos = buttonObject.transform.position - new Vector3(0, _pushDistance, 0); // Se calcula la nueva distancia del boton
+        buttonObject.transform.position = Vector3.MoveTowards(buttonObject.transform.position, _pushedPos, _speed * Time.deltaTime); // Se actualiza la posición del botón
+        buttonObject.GetComponent<MeshRenderer>().material = onMaterial; 
+        _isPushed = true; // Se marca que el botón ya fue presionado
     }
 }
